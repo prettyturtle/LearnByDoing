28d442d8d2f3c16920681081d21f267a821b55a0
prettyturtle <unifconti@gmail.com>
2023-08-27 01:47:45 +0900

    Initial Commit

---
+++ yc_LearnByDoing/yc_LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_LearnByDoingApp.swift
+//  yc_LearnByDoing
+//
+//  Created by yc on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct yc_LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
